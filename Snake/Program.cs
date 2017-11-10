da8603e750657c1a8cb362c8dbdd5cd8dc01043b
Pavel340 <zinovev-pasha@mail.ru>
2017-11-10 10:06:17 +0300

    Переделал на ООП

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,25 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sim1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            Draw(x1, y1, sim1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sim2 = '#';
-
-            Draw(x2, y2, sim2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sim);
         }
     }
 }
