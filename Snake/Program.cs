cac142c11f2a69917fd5ba5ae802bb2eb1cfb268
Pavel340 <zinovev-pasha@mail.ru>
2017-11-15 10:30:51 +0300

    Добавлены классы VerticalLine и HorizontalLine

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,11 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            /*Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            p2.Draw();*/
+
+            HorizontalLine line1 = new HorizontalLine(3, 19, 5, '+');
+            line1.Drow();
+
+            HorizontalLine line2 = new HorizontalLine(3, 19, 15, '+');
+            line2.Drow();
+
+            VerticalLine vLine1 = new VerticalLine(2, 5, 15, '-');
+            vLine1.VerticalLineDrow();
+
+            VerticalLine vLine2 = new VerticalLine(20, 5, 15, '-');
+            vLine2.VerticalLineDrow();
 
             Console.ReadLine();
         }
